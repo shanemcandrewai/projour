10fa983e4b5832e0d97a78d7f50979331be8f084
shane <s_mcandrew@yahoo.com>
2022-02-12 18:26:10 +0100

    variable cleanup

--- docs/scripts/tree.js
+++ docs/scripts/tree.js
@@ -1,8 +1,7 @@
 const toggler = document.getElementsByClassName('caret');
-let i;
 
-for (i = 0; i < toggler.length; i += 1) {
-  toggler[i].addEventListener('click', function togclick() {
+for (let i = 0; i < toggler.length; i += 1) {
+  toggler[i].addEventListener('click', function toggle() {
     this.parentElement.querySelector('.nested').classList.toggle('active');
     this.classList.toggle('caret-down');
   });
