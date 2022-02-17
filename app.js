d9786f019b6a943002f5bfbc278431f7fe52b572
shane <s_mcandrew@yahoo.com>
2022-02-17 20:26:27 +0100

    add express-session

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-import express from 'express';
 import Debug from 'debug';
-import { readFile } from 'fs/promises';
+import express from 'express';
+import { readFile, writeFile } from 'fs/promises';
+import session from 'express-session';
 
 const debug = Debug('app');
 const app = express();
 const port = 3000;
 
-const sec = async () => {
+const getSec = async () => {
+  let sec;
   try {
     debug('xxx reading');
-    return await readFile('sessionSecret.txt', 'utf8');
-  } catch (err) {
-    debug('xxx read failed');
-    return err;
+    sec = await readFile('sessSec.txt', 'utf8');
+  } catch (errRead) {
+    debug(errRead);
+    sec = (Math.random() + 1).toString(36).substring(3);
+    try {
+      await writeFile('sessSec.txt', sec);
+    } catch (errWrite) {
+      debug(errWrite);
+    }
   }
+  return sec;
 };
 
 app.use(express.static('docs'));
 
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: await getSec(),
+}));
+
+app.use((req, res, next) => {
+  if (!req.session.views) {
+    req.session.views = 0;
+  }
+  req.session.views += 1;
+  debug('xxx', req.session.views);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/foo', (req, res) => {
+  res.send(`you viewed this page ${req.session.views} times`);
 });
 
 app.listen(port, () => {
