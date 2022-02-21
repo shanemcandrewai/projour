9342caa2ab7f67d9043be78d402fbd2a813f5bcc
shane <s_mcandrew@yahoo.com>
2022-02-21 09:10:29 +0100

    add helmet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { readFile, writeFile } from 'fs/promises';
 import path from 'path';
 import session from 'express-session';
+import helmet from 'helmet';
 import sfs from 'session-file-store';
 import pbp from 'pbkdf2-password';
 
@@ -14,6 +15,7 @@ const port = 3000;
 
 // middleware
 
+app.use(helmet());
 app.use(express.static('docs'));
 app.use(express.urlencoded({ extended: false }));
 
@@ -38,7 +40,8 @@ const sess = {
   saveUninitialized: false, // don't create session until something stored
   secret: await getSec(),
   cookie: {},
-  store: new FileStore({}),
+  store: new FileStore(),
+  name: 'sessionId',
 };
 
 if (app.get('env') === 'production') {
@@ -51,6 +54,7 @@ app.use(session(sess));
 // Session-persisted message middleware
 
 app.use((req, res, next) => {
+  debug('xxx Session-persisted message middleware');
   const err = req.session.error;
   const msg = req.session.success;
   delete req.session.error;
@@ -65,7 +69,7 @@ app.use((req, res, next) => {
 // Authenticate using our user with server file
 const authenticate = async (name, passw, fn, authfile = 'auth.json') => {
   try {
-    debug('authenticating %s:%s', name, passw);
+    // debug('authenticating %s:%s', name, passw);
     const user = JSON.parse(await readFile(authfile, 'utf8'))[name];
     // query the db for the given username
     if (!user) return fn(null, null);
@@ -84,6 +88,7 @@ const authenticate = async (name, passw, fn, authfile = 'auth.json') => {
 };
 
 const restrict = (req, res, next) => {
+  debug('xxx restrict');
   if (req.session.user) {
     next();
   } else {
@@ -103,6 +108,7 @@ app.get('/restricted', restrict, (req, res) => {
 app.get('/logout', (req, res) => {
   // destroy the user's session to log them out
   // will be re-created next request
+  debug('xxx destroy');
   req.session.destroy(() => {
     res.redirect('/');
   });
@@ -119,6 +125,7 @@ app.post('/login', (req, res, next) => {
     if (user) {
       // Regenerate session when signing in
       // to prevent fixation
+      debug('xxx regenerate');
       req.session.regenerate(() => {
         // Store the user's primary key
         // in the session store to be retrieved,
@@ -129,6 +136,7 @@ app.post('/login', (req, res, next) => {
           + ' You may now access <a href="/restricted">/restricted</a>.';
         res.redirect('back');
       });
+      debug('xxx regenerate finished');
     } else {
       req.session.error = 'Authentication failed, please check your '
         + ' username and password.'
