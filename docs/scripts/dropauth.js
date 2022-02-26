26b0c02e78c9057c12b5c2f3a1d3aa428f074626
shane <s_mcandrew@yahoo.com>
2022-02-26 22:52:15 +0100

    add redirection

--- docs/scripts/dropauth.js
+++ docs/scripts/dropauth.js
@@ -1,13 +1,26 @@
 // Example POST method implementation:
 /* eslint-disable no-console */
 
-const getCode = async (url) => {
-  try {
-    const response = await fetch(url, { mode: 'no-cors' });
-    console.log(response);
-  } catch (err) {
-    console.log(err);
-  }
+const encodeQueryData = (data) => {
+  const ret = [];
+  Object.keys(data).forEach((d) => ret.push(`${encodeURIComponent(d)}=${encodeURIComponent(data[d])}`));
+  return ret.join('&');
 };
 
-getCode('https://www.dropbox.com/oauth2/authorize?client_id=qz0krgp7dd3kom6&response_type=code');
+const getCode = async (url) => {
+  const params = {
+    client_id: 'qz0krgp7dd3kom6',
+    response_type: 'code',
+    redirect_uri: 'http://localhost:3000/redir.html',
+  };
+
+  window.location.replace(url + encodeQueryData(params));
+  // try {
+  // const response = await fetch(url, { mode: 'cors' });
+  // console.log('xxx', response);
+  // } catch (err) {
+  // console.log(err);
+  // }
+};
+
+getCode('https://www.dropbox.com/oauth2/authorize?');
