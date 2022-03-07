969aa477cb058fc1e1a67c5541c4e1f0a38d1aeb
shane <s_mcandrew@yahoo.com>
2022-03-07 22:56:16 +0100

    switch to PKCE

--- docs/scripts/dropauth.js
+++ docs/scripts/dropauth.js
@@ -8,12 +8,22 @@ const encodeQueryData = (data) => {
 };
 
 const getCode = async (url) => {
+  // const rand = (Math.random() + 1).toString(36).substring(2);
+  const rand = 'dasfljf433dasfljf433dasfljf433dasfljf433dasfljf433';
+  const msgUint8 = new TextEncoder().encode(rand);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  // const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+  const hashHex = 'dhMhX1tsGrstwVVsMeWJfWe5Sfvt7rPg7JCG-sKUdU0';
+
   const params = {
-    client_id: 'qz0krgp7dd3kom6',
+    client_id: '2p3gyy8gc50oe4s',
     response_type: 'code',
     redirect_uri: 'http://localhost:3000/redir.html',
+    code_challenge: hashHex,
+    code_challenge_method: 'S256',
   };
-
+  // console.log('xxx code_challenge', params.code_challenge);
   window.location.replace(url + encodeQueryData(params));
   // try {
   // const response = await fetch(url, { mode: 'cors' });
--- docs/scripts/redir.js
+++ docs/scripts/redir.js
@@ -4,48 +4,59 @@
 // make query string for https://api.dropbox.com/oauth2/token
 // fetch post to retrieve access_token
 
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      // 'Content-Type': 'application/json',
-      'Content-Type': 'application/octet-stream',
-      Authorization: 'Bearer sl.BCreLYbKQE3elibcos2ZyDIWqHihTvwFTgC7qAbHptpfV1wtQmXl31jRyPDCZzbzeHibS1ykqQGsP9d85uNTavJba2QX76O-aMGXLW2VVfzMPhCoAAOO1XwZ6VuhU5ChUIpYuzmJ',
-      'Dropbox-API-Arg': '{"path":"/tf.txt"}',
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
+const getToken = async (params, url = 'https://api.dropboxapi.com/oauth2/token') => {
+  try {
+    const fetchResp = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(params),
+    });
+    // console.log('fetchResp', fetchResp);
+    return fetchResp.json();
+  } catch (err) {
+    console.log('xxx1');
+    console.log(err);
+  }
+  return 'getToken error';
+};
 
-async function getToken(url, app) {
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams(app), // body data type must match
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-// postData('https://content.dropboxapi.com/2/files/upload', { answer: 42 })
-// .then((data) => {
-// console.log(data); // JSON data parsed by `data.json()` call
-// });
+const upload = async (token, data, url = 'https://content.dropboxapi.com/2/files/upload') => {
+  try {
+    const fetchResp = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Dropbox-API-Arg': '{"path":"/tf.txt"}',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    });
+    return fetchResp.json();
+  } catch (err) {
+    console.log('xxx2');
+    console.log(err);
+  }
+  return 'upload error';
+};
 
-const params = new URLSearchParams(window.location.search);
-console.log('xxx', params.get('code'));
-getToken(
-  'https://api.dropboxapi.com/oauth2/token',
-  {
-    code: params.get('code'),
-    grant_type: 'authorization_code',
-    client_id: 'qz0krgp7dd3kom6',
-    client_secret: '94ubpfvyp2dvoye',
-    redirect_uri: 'http://localhost:3000/redir.html',
-  },
-)
-  .then((data) => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
+const test = async () => {
+  try {
+    const tokenResp = await getToken(
+      {
+        code: new URLSearchParams(window.location.search).get('code'),
+        grant_type: 'authorization_code',
+        client_id: '2p3gyy8gc50oe4s',
+        code_verifier: 'dasfljf433dasfljf433dasfljf433dasfljf433dasfljf433',
+        redirect_uri: 'http://localhost:3000/redir.html',
+      },
+    );
+    console.log('tokenResp', tokenResp);
+    return await upload(tokenResp.access_token, { answer: 42 });
+  } catch (err) {
+    console.log('xxx3');
+    console.log(err);
+  }
+  return 'test error';
+};
+
+console.log(test());
