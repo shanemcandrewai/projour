36054dd8f29a6640944916e41787ddaf6af4ba52
shane <s_mcandrew@yahoo.com>
2022-03-10 08:39:18 +0100

    fix connections

--- testredis.js
+++ testredis.js
@@ -2,12 +2,13 @@ import { createClient } from 'redis';
 (async () => {
   const client = createClient({
     url: 'redis://redis-11092.c250.eu-central-1-1.ec2.cloud.redislabs.com:11092',
-    username: 'admin',
-    password: 'k3.rXxv4',
+    username: [user]
+    password: [password],
   });
   client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
-  await client.set('key', 'value');
-  const value = await client.get('key');
+  await client.set('key1', 'value1');
+  const value = await client.get('key1');
+  console.log('xxx', value);
   await client.quit();
 })();
