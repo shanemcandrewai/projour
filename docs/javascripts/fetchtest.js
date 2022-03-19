1a08c6cbff30d4c081a9539c95b3cdd677f519dc
shane <s_mcandrew@yahoo.com>
2022-03-19 15:14:09 +0100

    so question

---
+++ docs/javascripts/fetchtest.js
@@ -0,0 +1,43 @@
+const fetchAsync = async () => {
+  const url = 'https://httpbin.org/post';
+  const data = { node1: 'val1', node2: 'val2' };
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const ret = await response.json();
+    document.getElementById('status').innerHTML = 'success';
+    document.getElementById('return').innerHTML = ret.data;
+  } catch (error) {
+    document.getElementById('status').innerHTML = 'failed';
+    document.getElementById('return').innerHTML = error;
+  }
+};
+
+const fetchAsyncDefault = async (
+  url = 'https://httpbin.org/post',
+  data = { node1: 'val1', node2: 'val2' },
+) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const ret = await response.json();
+    document.getElementById('status').innerHTML = 'success';
+    document.getElementById('return').innerHTML = ret.data;
+  } catch (error) {
+    document.getElementById('status').innerHTML = 'failed';
+    document.getElementById('return').innerHTML = error;
+  }
+};
+
+document.getElementById('fetchAsync').addEventListener('click', fetchAsync);
+document.getElementById('fetchAsyncDefault').addEventListener('click', fetchAsyncDefault);
--- docs/javascripts/tree.js
+++
@@ -1,8 +0,0 @@
-const toggler = document.getElementsByClassName('caret');
-
-for (let i = 0; i < toggler.length; i += 1) {
-  toggler[i].addEventListener('click', function toggle() {
-    this.parentElement.querySelector('.nested').classList.toggle('active');
-    this.classList.toggle('caret-down');
-  });
-}
