63100b7757231126cf75fc6de29bf4244b312dd6
shane <s_mcandrew@yahoo.com>
2022-03-19 10:10:09 +0100

    httpbin.js working

--- docs/javascripts/httpbin.js
+++ docs/javascripts/httpbin.js
@@ -1,21 +1,24 @@
-const loginPost = async () => {
-  const data = { username: 'example' };
+const loginPost = async (url = 'https://httpbin.org/post', data = {
+  url: document.getElementById('floatingUrl').value,
+  user: document.getElementById('floatingUser').value,
 
-  fetch('https://httpbin.org/post', {
-    method: 'POST', // or 'PUT'
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((ret) => {
-      document.getElementById('message').innerHTML = JSON.stringify(ret.data);
-      // console.log('Success:', data);
-    })
-    .catch((error) => {
-      document.getElementById('message').innerHTML = error;
-      // console.error('Error:', error);
+}) => {
+  // const data = { username: 'example' };
+  try {
+    const response = await fetch('https://httpbin.org/post', {
+    // const response = await fetch(url, {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
     });
+    const ret = await response.json();
+    document.getElementById('message').innerHTML = JSON.stringify(ret.data);
+    document.getElementById('from').innerHTML = 'success';
+  } catch (error) {
+    document.getElementById('message').innerHTML = error;
+    document.getElementById('from').innerHTML = 'failed';
+  }
 };
 document.getElementById('demo').addEventListener('click', loginPost);
