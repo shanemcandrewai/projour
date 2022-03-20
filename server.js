a12c13a516e48068b655643bd28db142bcce7317
shane <s_mcandrew@yahoo.com>
2022-03-20 13:04:59 +0100

    prettyPrint winston

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ if (process.env.NODE_ENV !== 'production') {
   logger.add(new transports.Console({
     format: format.combine(
       format.colorize(),
-      format.simple(),
+      // format.simple(),
+      format.prettyPrint(),
     ),
   }));
 }
