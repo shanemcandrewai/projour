cd7ba5f03146049fd9481f947a6727f6e70cc939
shane <s_mcandrew@yahoo.com>
2022-03-30 20:43:07 +0200

    fix carets

--- docs/scripts/tree.js
+++ docs/scripts/tree.js
@@ -28,20 +28,23 @@ treeVars.h1.append('Dynamic ML list');
 document.body.append(treeVars.h1);
 
 treeVars.ul0 = document.createElement('ul');
-treeVars.ul0.setAttribute('is', 'expanding-list');
 document.body.append(treeVars.ul0);
 
 const addNode = (parent, level = 1) => {
   Object.entries(parent).forEach((child) => {
     treeVars[`li${level}`] = document.createElement('li');
-    treeVars[`li${level}`].append(child[0]);
     treeVars[`ul${level - 1}`].append(treeVars[`li${level}`]);
     if (Object.keys(child[1]).length) {
+      treeVars[`span${level}`] = document.createElement('span');
+      treeVars[`span${level}`].classList.add('caret');
+      treeVars[`span${level}`].append(child[0]);
+      treeVars[`li${level}`].append(treeVars[`span${level}`]);
       treeVars[`ul${level}`] = document.createElement('ul');
-      treeVars[`li${level}`].append(treeVars[`ul${level}`]);
       treeVars[`ul${level}`].classList.add('nested');
-      treeVars[`li${level}`].classList.add('caret');
+      treeVars[`li${level}`].append(treeVars[`ul${level}`]);
       addNode(child[1], level + 1);
+    } else {
+      treeVars[`li${level}`].append(child[0]);
     }
   });
 };
@@ -49,13 +52,8 @@ const addNode = (parent, level = 1) => {
 addNode(jsn);
 
 const toggler = document.getElementsByClassName('caret');
-
 const toggle = (ind) => {
-  console.log(ind);
-  console.log(toggler[ind].parentElement.querySelector('.nested'));
-
   toggler[ind].parentElement.querySelector('.nested').classList.toggle('active');
-
   toggler[ind].classList.toggle('caret-down');
 };
 for (let i = 0; i < toggler.length; i += 1) {
