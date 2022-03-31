80fee20f3d38168fd1e0212026defd448701066a
shane <s_mcandrew@yahoo.com>
2022-03-31 16:35:02 +0200

    remove span wip

--- docs/scripts/tree.js
+++ docs/scripts/tree.js
@@ -22,29 +22,25 @@ const jsn = {
 };
 
 const treeVars = {};
-
-treeVars.h1 = document.createElement('h1');
-treeVars.h1.append('Dynamic ML list');
-document.body.append(treeVars.h1);
-
+const mllist = document.getElementById('ml-list');
 treeVars.ul0 = document.createElement('ul');
-document.body.append(treeVars.ul0);
+mllist.append(treeVars.ul0);
 
 const addNode = (parent, level = 1) => {
   Object.entries(parent).forEach((child) => {
     treeVars[`li${level}`] = document.createElement('li');
+    treeVars[`li${level}`].append(child[0]);
     treeVars[`ul${level - 1}`].append(treeVars[`li${level}`]);
     if (Object.keys(child[1]).length) {
-      treeVars[`span${level}`] = document.createElement('span');
+      /*       treeVars[`span${level}`] = document.createElement('span');
       treeVars[`span${level}`].classList.add('caret');
       treeVars[`span${level}`].append(child[0]);
-      treeVars[`li${level}`].append(treeVars[`span${level}`]);
+      treeVars[`li${level}`].append(treeVars[`span${level}`]); */
+      treeVars[`li${level}`].classList.add('caret');
       treeVars[`ul${level}`] = document.createElement('ul');
-      treeVars[`ul${level}`].classList.add('nested');
+      treeVars[`ul${level}`].classList.add('closed');
       treeVars[`li${level}`].append(treeVars[`ul${level}`]);
       addNode(child[1], level + 1);
-    } else {
-      treeVars[`li${level}`].append(child[0]);
     }
   });
 };
@@ -53,10 +49,13 @@ addNode(jsn);
 
 const toggler = document.getElementsByClassName('caret');
 const toggle = (ind) => {
-  // toggler[ind].parentElement.querySelector('.nested').classList.toggle('active');
-  toggler[ind].nextElementSibling.classList.toggle('active');
+  console.log(ind, toggler[ind].querySelector('UL'));
+  toggler[ind].querySelector('UL').classList.toggle('open');
   toggler[ind].classList.toggle('caret-down');
 };
 for (let i = 0; i < toggler.length; i += 1) {
   toggler[i].addEventListener('click', () => toggle(i));
 }
+
+// const dropdownMacos = document.getElementById('dropdownMacos');
+// dropdownMacos.addEventListener('click', () => console.log('dropdownMacos.setAttribute'));
