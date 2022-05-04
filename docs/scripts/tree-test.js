5c3389fb234346344488c06628049741e52c04b0
shane <s_mcandrew@yahoo.com>
2022-05-04 18:18:49 +0200

    added toggle

--- docs/scripts/tree-test.js
+++ docs/scripts/tree-test.js
@@ -22,6 +22,12 @@ const jsn = [
   },
 ];
 
+const toggle = (event, elem) => {
+  elem.querySelector('UL').style.display = 'none';
+  elem.classList.toggle('caret-down');
+  event.stopPropagation();
+};
+
 const addNodes = (parent, fragm) => {
   const ul = document.createElement('ul');
   fragm.appendChild(ul);
@@ -30,6 +36,8 @@ const addNodes = (parent, fragm) => {
     li.textContent = key;
     ul.appendChild(li);
     if (typeof value === 'object' && Object.keys(value).length) {
+      li.classList.add('caret');
+      li.addEventListener('click', (event) => toggle(event, li));
       return addNodes(value, li);
     }
     li.textContent += `: ${value}`;
@@ -41,9 +49,9 @@ const addNodes = (parent, fragm) => {
 const list = document.querySelector('#ml-list');
 const frag = addNodes(jsn, new DocumentFragment());
 
-const cn = frag.children;
+/* const cn = frag.children;
 for (let i = 0, len = cn.length; i < len; i += 1) {
   console.log(cn[i]);
-}
+} */
 
 list.appendChild(frag);
